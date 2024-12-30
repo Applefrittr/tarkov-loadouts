bfbaec8b25e40186b799c3c2ac58d97ac83279e8
applefrittr <jacob.lee.jordan@gmail.com>
2024-12-30 17:55:55 -0500

    fix for compatibility between Helmet and Headphone data objects in Loadout.js

--- src/api/tarkov-dev.js
+++ src/api/tarkov-dev.js
@@ -15,6 +15,9 @@ export async function getTarkovData() {
                 types
                 description
                 blocksHeadphones
+                conflictingItems {
+                  name
+                }
                 inspectImageLink
                 buyFor {
                   price
