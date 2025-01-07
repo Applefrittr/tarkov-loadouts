ea91fd0dadd5cb74f818fd6800f312201fc4d26e
applefrittr <jacob.lee.jordan@gmail.com>
2025-01-07 12:00:49 -0500

    hideout logo refactored from psuedo element of body into element in RouteSwitch

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -2,13 +2,22 @@ import { BrowserRouter } from "react-router-dom";
 import RoutePaths from "./Components/RoutePaths.js";
 import Nav from "./Components/Nav";
 import { motion } from "framer-motion";
+import Logo from "./Assets/hideout-logo.png";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter basename="/tarkov-loadouts">
-      <motion.section initial={{opacity:0}} animate={{opacity:1}} transition={{duration: .5, delay: 1.5}}>
+      <motion.section
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 1.5 }}
+        id="app-container"
+      >
         <Nav />
         <RoutePaths />
+        <div id="hideout-logo">
+          <img src={Logo} alt="logo" />
+        </div>
       </motion.section>
     </BrowserRouter>
   );
