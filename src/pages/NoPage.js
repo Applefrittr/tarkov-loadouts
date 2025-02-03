47c6df6dff0ce2dfbfc32cc5c8be151085b7c863
applefrittr <jacob.lee.jordan@gmail.com>
2025-02-02 22:03:43 -0500

    styling and layout added to NoPage.js

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+import "../Styles/NoPage.css";
+
 const NoPage = () => {
   return (
-    <section>
-      <div>Nothing here but death</div>
+    <section id="nopage-container">
+      <div className="nopage-elements">
+        <h2>It's safe inside the Hideout</h2>
+        <p>404 - Page not found</p>
+        <Link to="/" id="getGear-button" className="nopage-btn">
+          Return
+        </Link>
+      </div>
     </section>
   );
 };
